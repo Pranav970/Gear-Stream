a4dc76158eeed0d9327e4d17c972a22b496cece5
Pranav Rajput <92659078+Pranav970@users.noreply.github.com>
2026-05-06 20:40:19 +0530

    Update Transaction.scala

--- fraud-pipeline/src/main/scala/com/fraudpipeline/models/Transaction.scala
+++ fraud-pipeline/src/main/scala/com/fraudpipeline/models/Transaction.scala
@@ -1,1 +1,10 @@
+package com.fraudpipeline.models
 
+case class Transaction(
+  transactionId: String,
+  accountId: String,
+  amount: Double,
+  timestamp: String, // ISO-8601 string
+  merchantId: String,
+  location: String
+)
